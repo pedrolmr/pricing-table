f0ced645ccc6e5ee908804ba155fea302de26df0
pedrolmr <pedroluismontesinosrivera@gmail.com>
2021-09-05 02:59:26 -0400

    Info changes depending on what button you press on mobile

--- script.js
+++ script.js
@@ -5,44 +5,75 @@ let headerBtnBusiness = document.querySelector(
 let headerBtnPro = document.querySelector(".header .row .col .btn-pro");
 let footerCol = document.querySelectorAll(".footer .row .col");
 
-console.log(footerCol[1]);
-console.log(footerCol[2]);
-console.log(footerCol[3]);
+// let topButtons = document.querySelector(".header .row .col");
+// console.log("Top Buttons", topButtons);
+
+// console.log(footerCol[1]);
+// console.log(footerCol[2]);
+// console.log(footerCol[3]);
+
+console.log("header basic button:", headerBtnBasic.classList[3]);
+console.log("header business button:", headerBtnBusiness.classList[3]);
+console.log("header pro button:", headerBtnPro.classList[3]);
 
 let table = document.querySelector(".table");
 console.log("TABLE:", table);
 
-let basicInfo = document.querySelector(".basic");
-let businessInfo = document.querySelector(".business");
-let proInfo = document.querySelector(".pro");
+let basicInfo = document.querySelectorAll(".basic");
+let businessInfo = document.querySelectorAll(".business");
+let proInfo = document.querySelectorAll(".pro");
 
 console.log("BASICINFO:", basicInfo);
 console.log("BUSINESSINFO:", businessInfo);
 console.log("PROINFO:", proInfo);
 
-console.log(footerCol[0].nextSibling.nextSibling.childNodes[1].classList[3]);
-console.log(footerCol[1].nextSibling.nextSibling.childNodes[1].classList[3]);
-console.log(footerCol[2].nextSibling.nextSibling.childNodes[1].classList[3]);
+// console.log(
+//   "classessss",
+//   footerCol[0].nextSibling.nextSibling.childNodes[1].classList[3]
+// );
+// console.log(
+//   "classessss",
+//   footerCol[1].nextSibling.nextSibling.childNodes[1].classList[3]
+// );
+// console.log(
+//   "classessss",
+//   footerCol[2].nextSibling.nextSibling.childNodes[1].classList[3]
+// );
 
-// for (let i = 0; i < footerCol.length; i++) {
-//   console.log(footerCol[i].childNodes[1]);
-// }
-// console.log(footerCol.childNodes);
+// const topButtons = [
+//   headerBtnBasic.classList[3],
+//   headerBtnBusiness.classList[3],
+//   headerBtnPro.classList[3],
+// ];
 
 headerBtnBasic.addEventListener("click", function () {
   //REMOVE THE FIRST, THIRD AND FOURTH BOTTOM BUTTONS AND SHOW THE BASIC BUTTON:
 
   footerCol[0].style.display = "none";
   footerCol[1].style.display = "block";
-
   footerCol[2].style.display = "none";
   footerCol[3].style.display = "none";
 
   //REMOVE THE BUSINESS AND PRO TBODIES FROM THE PAGE AND SHOW THE "BASIC" CONTENT
-  basicInfo.style.display = "block";
-  basicInfo.style.width = "100%";
-  businessInfo.style.display = "none";
-  proInfo.style.display = "none";
+
+  for (let i = 0; i < basicInfo.length; i++) {
+    if (basicInfo[i].classList[0] == "basic") {
+      basicInfo[i].style.display = "block";
+      basicInfo[i].style.width = "100%";
+    }
+  }
+
+  for (let j = 0; j < businessInfo.length; j++) {
+    if (businessInfo[j].classList[0] == "business") {
+      businessInfo[j].style.display = "none";
+    }
+  }
+
+  for (let k = 0; k < proInfo.length; k++) {
+    if (proInfo[k].classList[0] == "pro") {
+      proInfo[k].style.display = "none";
+    }
+  }
 });
 
 headerBtnBusiness.addEventListener("click", function () {
@@ -53,11 +84,24 @@ headerBtnBusiness.addEventListener("click", function () {
   footerCol[2].style.display = "block";
   footerCol[3].style.display = "none";
   //REMOVE THE BASIC AND PRO TBODIES FROM THE PAGE AND SHOW THE "BUSINESS" CONTENT
+  for (let i = 0; i < basicInfo.length; i++) {
+    if (basicInfo[i].classList[0] == "basic") {
+      basicInfo[i].style.display = "none";
+    }
+  }
 
-  basicInfo.style.display = "none";
-  businessInfo.style.display = "block";
-  businessInfo.style.width = "100%";
-  proInfo.style.display = "none";
+  for (let j = 0; j < businessInfo.length; j++) {
+    if (businessInfo[j].classList[0] == "business") {
+      businessInfo[j].style.display = "block";
+      businessInfo[j].style.width = "100%";
+    }
+  }
+
+  for (let k = 0; k < proInfo.length; k++) {
+    if (proInfo[k].classList[0] == "pro") {
+      proInfo[k].style.display = "none";
+    }
+  }
 
   console.log("business");
 });
@@ -71,11 +115,24 @@ headerBtnPro.addEventListener("click", function () {
   footerCol[3].style.display = "block";
 
   //REMOVE THE BASIC AND BUSINESS TBODIES FROM THE PAGE AND SHOW THE "PRO" CONTENT
+  for (let i = 0; i < basicInfo.length; i++) {
+    if (basicInfo[i].classList[0] == "basic") {
+      basicInfo[i].style.display = "none";
+    }
+  }
 
-  basicInfo.style.display = "none";
-  businessInfo.style.display = "none";
-  proInfo.style.display = "block";
-  proInfo.style.width = "100%";
+  for (let j = 0; j < businessInfo.length; j++) {
+    if (businessInfo[j].classList[0] == "business") {
+      businessInfo[j].style.display = "none";
+    }
+  }
+
+  for (let k = 0; k < proInfo.length; k++) {
+    if (proInfo[k].classList[0] == "pro") {
+      proInfo[k].style.display = "block";
+      proInfo[k].style.width = "100%";
+    }
+  }
 
   console.log("pro");
 });
