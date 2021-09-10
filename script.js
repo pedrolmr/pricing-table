152d32bc083c58b2536e377e6c77d19590ae365b
pedrolmr <pedroluismontesinosrivera@gmail.com>
2021-09-10 05:59:26 -0400

    columns finally appear after the screen size increases, some styles are broken tho

--- script.js
+++ script.js
@@ -5,134 +5,208 @@ let headerBtnBusiness = document.querySelector(
 let headerBtnPro = document.querySelector(".header .row .col .btn-pro");
 let footerCol = document.querySelectorAll(".footer .row .col");
 
-// let topButtons = document.querySelector(".header .row .col");
-// console.log("Top Buttons", topButtons);
-
-// console.log(footerCol[1]);
-// console.log(footerCol[2]);
-// console.log(footerCol[3]);
-
-console.log("header basic button:", headerBtnBasic.classList[3]);
-console.log("header business button:", headerBtnBusiness.classList[3]);
-console.log("header pro button:", headerBtnPro.classList[3]);
-
-let table = document.querySelector(".table");
-console.log("TABLE:", table);
-
 let basicInfo = document.querySelectorAll(".basic");
 let businessInfo = document.querySelectorAll(".business");
 let proInfo = document.querySelectorAll(".pro");
 
-console.log("BASICINFO:", basicInfo);
-console.log("BUSINESSINFO:", businessInfo);
-console.log("PROINFO:", proInfo);
+const mediaQuery = window.matchMedia("(max-width: 700px)");
 
-// console.log(
-//   "classessss",
-//   footerCol[0].nextSibling.nextSibling.childNodes[1].classList[3]
-// );
-// console.log(
-//   "classessss",
-//   footerCol[1].nextSibling.nextSibling.childNodes[1].classList[3]
-// );
-// console.log(
-//   "classessss",
-//   footerCol[2].nextSibling.nextSibling.childNodes[1].classList[3]
-// );
+function handleMobileChange(e) {
+  if (e.matches) {
+    footerCol[0].style.display = "none";
+    footerCol[1].style.display = "block";
+    footerCol[2].style.display = "none";
+    footerCol[3].style.display = "none";
 
-// const topButtons = [
-//   headerBtnBasic.classList[3],
-//   headerBtnBusiness.classList[3],
-//   headerBtnPro.classList[3],
-// ];
+    for (let i = 0; i < basicInfo.length; i++) {
+      if (basicInfo[i].classList[0] == "basic") {
+        basicInfo[i].style.display = "block";
+        // basicInfo[i].style.visibility = "visible";
+        basicInfo[i].style.width = "100%";
+        // console.log("BASIC INFO:", basicInfo[i]);
+      }
+    }
 
-headerBtnBasic.addEventListener("click", function () {
-  //REMOVE THE FIRST, THIRD AND FOURTH BOTTOM BUTTONS AND SHOW THE BASIC BUTTON:
+    for (let j = 0; j < businessInfo.length; j++) {
+      if (businessInfo[j].classList[0] == "business") {
+        businessInfo[j].style.display = "none";
+        // businessInfo[j].style.visibility = "hidden";
+      }
+    }
 
-  footerCol[0].style.display = "none";
-  footerCol[1].style.display = "block";
-  footerCol[2].style.display = "none";
-  footerCol[3].style.display = "none";
+    for (let k = 0; k < proInfo.length; k++) {
+      if (proInfo[k].classList[0] == "pro") {
+        proInfo[k].style.display = "none";
+        // proInfo[k].style.visibility = "hidden";
+      }
+    }
 
-  //REMOVE THE BUSINESS AND PRO TBODIES FROM THE PAGE AND SHOW THE "BASIC" CONTENT
+    headerBtnBasic.addEventListener("click", function () {
+      //REMOVE THE FIRST, THIRD AND FOURTH BOTTOM BUTTONS AND SHOW THE BASIC BUTTON:
 
-  for (let i = 0; i < basicInfo.length; i++) {
-    if (basicInfo[i].classList[0] == "basic") {
-      basicInfo[i].style.display = "block";
-      basicInfo[i].style.width = "100%";
+      footerCol[0].style.display = "none";
+      footerCol[1].style.display = "block";
+      footerCol[2].style.display = "none";
+      footerCol[3].style.display = "none";
+
+      for (let i = 0; i < basicInfo.length; i++) {
+        if (basicInfo[i].classList[0] == "basic") {
+          basicInfo[i].style.display = "block";
+          // basicInfo[i].style.visibility = "visible";
+          // basicInfo[i].classList.add("show-on-mobile");
+
+          // basicInfo[i].className += " show-on-mobile";
+          basicInfo[i].style.width = "100%";
+
+          // basicInfo[i].style.width = "100%";
+
+          console.log("BASIC INFO:", basicInfo[i]);
+        }
+      }
+
+      for (let j = 0; j < businessInfo.length; j++) {
+        if (businessInfo[j].classList[0] == "business") {
+          businessInfo[j].style.display = "none";
+          // businessInfo[j].style.visibility = "hidden";
+        }
+      }
+
+      for (let k = 0; k < proInfo.length; k++) {
+        if (proInfo[k].classList[0] == "pro") {
+          proInfo[k].style.display = "none";
+          // proInfo[k].style.visibility = "hidden";
+        }
+      }
+    });
+
+    headerBtnBusiness.addEventListener("click", function () {
+      //REMOVE THE FIRST, SECOND AND FOURTH BOTTOM BUTTONS:
+
+      footerCol[0].style.display = "none";
+      footerCol[1].style.display = "none";
+      footerCol[2].style.display = "block";
+      footerCol[3].style.display = "none";
+      //REMOVE THE BASIC AND PRO TBODIES FROM THE PAGE AND SHOW THE "BUSINESS" CONTENT
+      for (let i = 0; i < basicInfo.length; i++) {
+        if (basicInfo[i].classList[0] == "basic") {
+          basicInfo[i].style.display = "none";
+          // basicInfo[i].style.visibility = "hidden";
+        }
+      }
+
+      for (let j = 0; j < businessInfo.length; j++) {
+        if (businessInfo[j].classList[0] == "business") {
+          businessInfo[j].style.display = "block";
+          // businessInfo[j].style.visibility = "visible";
+          // businessInfo[j].classList.add("show-on-mobile");
+          // businessInfo[j].className += " show-on-mobile";
+          businessInfo[j].style.width = "100%";
+
+          // businessInfo[j].style.width = "100%";
+          console.log("BUSINESS INFO:", businessInfo[j]);
+        }
+      }
+
+      for (let k = 0; k < proInfo.length; k++) {
+        if (proInfo[k].classList[0] == "pro") {
+          proInfo[k].style.display = "none";
+          // proInfo[k].style.visibility = "hidden";
+        }
+      }
+
+      console.log("business");
+    });
+
+    headerBtnPro.addEventListener("click", function () {
+      //REMOVES THE FIRST, SECOND AND THIRD BOTTOM BUTTONS:
+
+      footerCol[0].style.display = "none";
+      footerCol[1].style.display = "none";
+      footerCol[2].style.display = "none";
+      footerCol[3].style.display = "block";
+
+      //REMOVE THE BASIC AND BUSINESS TBODIES FROM THE PAGE AND SHOW THE "PRO" CONTENT
+      for (let i = 0; i < basicInfo.length; i++) {
+        if (basicInfo[i].classList[0] == "basic") {
+          basicInfo[i].style.display = "none";
+          // basicInfo[i].style.visibility = "hidden";
+        }
+      }
+
+      for (let j = 0; j < businessInfo.length; j++) {
+        if (businessInfo[j].classList[0] == "business") {
+          businessInfo[j].style.display = "none";
+          // businessInfo[j].style.visibility = "hidden";
+        }
+      }
+
+      for (let k = 0; k < proInfo.length; k++) {
+        if (proInfo[k].classList[0] == "pro") {
+          proInfo[k].style.display = "block";
+          // proInfo[k].style.visibility = "hidden";
+          // proInfo[k].classList.add("show-on-mobile");
+          // proInfo[k].className += " show-on-mobile";
+          proInfo[k].style.width = "100%";
+
+          // proInfo[k].style.width = "100%";
+
+          console.log("PRO INFO:", proInfo[k]);
+        }
+      }
+
+      console.log("pro");
+    });
+  } else {
+    footerCol[0].style.display = "flex";
+    footerCol[1].style.display = "flex";
+    footerCol[2].style.display = "flex";
+    footerCol[3].style.display = "flex";
+
+    footerCol[0].style.justifyContent = "center";
+    footerCol[0].style.alignItems = "center";
+
+    footerCol[1].style.justifyContent = "center";
+    footerCol[1].style.alignItems = "center";
+
+    footerCol[2].style.justifyContent = "center";
+    footerCol[2].style.alignItems = "center";
+
+    footerCol[3].style.justifyContent = "center";
+    footerCol[3].style.alignItems = "center";
+
+    console.log("basic info:", basicInfo);
+    console.log("business info:", businessInfo);
+    console.log("pro info:", proInfo);
+
+    for (let i = 0; i < basicInfo.length; i++) {
+      if (basicInfo[i].classList[0] == "basic") {
+        basicInfo[i].style.display = "";
+        // basicInfo[i].style.border = "3px solid red";
+        //set basicInfo to a row
+        basicInfo[i].style.width = "50%";
+
+        // basicInfo[i].style.width = "100%";
+        // console.log("BASIC INFO:", basicInfo[i]);
+      }
+    }
+
+    for (let j = 0; j < businessInfo.length; j++) {
+      if (businessInfo[j].classList[0] == "business") {
+        businessInfo[j].style.display = "";
+        businessInfo[j].style.width = "50%";
+        // businessInfo[j].style.border = "3px solid purple";
+      }
+    }
+
+    for (let k = 0; k < proInfo.length; k++) {
+      if (proInfo[k].classList[0] == "pro") {
+        proInfo[k].style.display = "";
+        proInfo[k].style.width = "50%";
+        // proInfo[k].style.border = "3px solid yellow";
+      }
     }
   }
+}
 
-  for (let j = 0; j < businessInfo.length; j++) {
-    if (businessInfo[j].classList[0] == "business") {
-      businessInfo[j].style.display = "none";
-    }
-  }
-
-  for (let k = 0; k < proInfo.length; k++) {
-    if (proInfo[k].classList[0] == "pro") {
-      proInfo[k].style.display = "none";
-    }
-  }
-});
-
-headerBtnBusiness.addEventListener("click", function () {
-  //REMOVE THE FIRST, SECOND AND FOURTH BOTTOM BUTTONS:
-
-  footerCol[0].style.display = "none";
-  footerCol[1].style.display = "none";
-  footerCol[2].style.display = "block";
-  footerCol[3].style.display = "none";
-  //REMOVE THE BASIC AND PRO TBODIES FROM THE PAGE AND SHOW THE "BUSINESS" CONTENT
-  for (let i = 0; i < basicInfo.length; i++) {
-    if (basicInfo[i].classList[0] == "basic") {
-      basicInfo[i].style.display = "none";
-    }
-  }
-
-  for (let j = 0; j < businessInfo.length; j++) {
-    if (businessInfo[j].classList[0] == "business") {
-      businessInfo[j].style.display = "block";
-      businessInfo[j].style.width = "100%";
-    }
-  }
-
-  for (let k = 0; k < proInfo.length; k++) {
-    if (proInfo[k].classList[0] == "pro") {
-      proInfo[k].style.display = "none";
-    }
-  }
-
-  console.log("business");
-});
-
-headerBtnPro.addEventListener("click", function () {
-  //REMOVES THE FIRST, SECOND AND THIRD BOTTOM BUTTONS:
-
-  footerCol[0].style.display = "none";
-  footerCol[1].style.display = "none";
-  footerCol[2].style.display = "none";
-  footerCol[3].style.display = "block";
-
-  //REMOVE THE BASIC AND BUSINESS TBODIES FROM THE PAGE AND SHOW THE "PRO" CONTENT
-  for (let i = 0; i < basicInfo.length; i++) {
-    if (basicInfo[i].classList[0] == "basic") {
-      basicInfo[i].style.display = "none";
-    }
-  }
-
-  for (let j = 0; j < businessInfo.length; j++) {
-    if (businessInfo[j].classList[0] == "business") {
-      businessInfo[j].style.display = "none";
-    }
-  }
-
-  for (let k = 0; k < proInfo.length; k++) {
-    if (proInfo[k].classList[0] == "pro") {
-      proInfo[k].style.display = "block";
-      proInfo[k].style.width = "100%";
-    }
-  }
-
-  console.log("pro");
-});
+mediaQuery.addListener(handleMobileChange);
+handleMobileChange(mediaQuery);
